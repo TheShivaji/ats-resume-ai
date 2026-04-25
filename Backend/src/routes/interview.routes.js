b37cc8eaeefeb9cd0fd98b5dfec76bed2c9f4e85
TheShivaji <shivajijagdale2005@gmail.com>
2026-04-25 12:59:46 +0530

    refactor: convert CommonJS require to ES module import

---
+++ Backend/src/routes/interview.routes.js
@@ -0,0 +1,43 @@
+import express from "express"
+import authMiddleware from "../middleware/auth.middleware.js"
+
+import upload from "../middleware/file.js"
+import { generateResumePdfController, getAllInterviewReportsController, getInterviewReportByIdController } from "../controllers/interview.controllers.js"
+
+const interviewRouter = express.Router()
+
+
+
+/**
+ * @route POST /api/interview/
+ * @description generate new interview report on the basis of user self description,resume pdf and job description.
+ * @access private
+ */
+interviewRouter.post("/", authMiddleware.authUser, upload.single("resume"), generateInterViewReportController)
+
+/**
+ * @route GET /api/interview/report/:interviewId
+ * @description get interview report by interviewId.
+ * @access private
+ */
+interviewRouter.get("/report/:interviewId", authMiddleware.authUser, getInterviewReportByIdController)
+
+
+/**
+ * @route GET /api/interview/
+ * @description get all interview reports of logged in user.
+ * @access private
+ */
+interviewRouter.get("/", authMiddleware.authUser, getAllInterviewReportsController)
+
+
+/**
+ * @route GET /api/interview/resume/pdf
+ * @description generate resume pdf on the basis of user self description, resume content and job description.
+ * @access private
+ */
+interviewRouter.post("/resume/pdf/:interviewReportId", authMiddleware.authUser, generateResumePdfController)
+
+
+
+export default interviewRouter
